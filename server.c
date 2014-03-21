09ab7a494dff6b8037ff33b4a6ae0f51ee5aca66
ZeroSobel <annoyingjk3@yahoo.com>
2014-03-21 18:26:49 -0500

    Updated server
    Now has switch and ACK. Needs checksum.

--- server.c
+++ server.c
@@ -13,9 +13,18 @@
 #include <unistd.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <stdint.h>
+#include <iostream>
 
 #define PACKET_LEN 128
 #define SERVER_PORT 10050
+
+#define ACK 0
+#define NAK 1
+#define GET 2
+#define PUT 3
+#define DEL 4
+#define TRN 5
 
 int main(int argc, char** argv)
 {
@@ -52,29 +61,101 @@ int main(int argc, char** argv)
     }
 
     printf("Successfully bound server to port %d and listening for clients...\n\n", SERVER_PORT);
-
+	
+	bool wait_put = true;
+	FILE *outfile;
+	bool send_ack = false;
+	
     while(1)
     {
+		uint8_t cur_seq = 0;
+		uint8_t exp_seq = 0;
+	
         if (recvfrom(sockfd, buffer, PACKET_LEN, 0, (struct sockaddr*)&client_addr, &slen)==-1)
 		{
 	    	fprintf(stderr, "Error: Could not receive from client\n");
 	    	close(sockfd);
 	    	exit(EXIT_FAILURE);
     	}
-
+		
+		//Getting packet sections
+		uint8_t * packet_type = (uint8_t*) buffer + 0;
+		uint8_t * seq_num = (uint8_t*) buffer + 1;
+		uint16_t * checksum = (uint16_t*) buffer + 2;
+		uint16_t * data_size = (uint16_t*) buffer + 4;
+		char* data = buffer + 6;
+		cur_seq = *seq_num;
+		
+		// Insert checksum here
+		
+		
+		
+		switch(packet_type) {
+			case ACK:
+				printf("Why are you receiving an ACK?");
+			break;
+			case NAK:
+				printf("Why are you receiving a NAK?");
+			break;
+			case GET:
+				printf("Why are you receiving a GET?");
+			break;
+			case PUT:
+				if(wait_put) {
+					outfile = fopen(data, "wb");
+					wait_put = false;
+					send_ack = true;
+				}
+				else {
+					printf("Another transfer already in progress.");
+					send_ack = false;
+				}
+			break;
+			case DEL:
+				printf("Why are you receiving a DEL?");
+			break;
+			case TRN:
+				if(!wait_put && exp_seq == *seq_num) {
+					if(data_size > 0) {
+						fwrite(data, 1, data_size, outfile);
+					}
+					else {
+						fclose(outfile);
+						wait_put = true;
+					}
+					send_ack = true;
+				}
+				else {
+					printf("Transfer has not yet been initialized.");
+					send_ack = false;
+				}
+			break;
+			default:
+				printf("Packet type not supported");
+			break;
+		}
+		
+		
         printf("Received packet from %s:%d\nData: %s\n",
                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buffer);
         
+		if(send_ack) {
+			printf("Sending ACK to client...\n");
 
-        printf("Sending ACK to client...\n");
-
-        snprintf(buffer, PACKET_LEN, "Acknoledged");
-        if (sendto(sockfd, buffer, PACKET_LEN, 0, (struct sockaddr*) &client_addr, slen) == -1)
-        {
-        	perror("Error: could not send acknowledge to client\n");
-        	close(sockfd);
-        	exit(EXIT_FAILURE);
-        }
+			snprintf(buffer, PACKET_LEN, "Acknowledged");
+			bzero(buffer, 128);
+			packet_type = 0;
+			seq_num = cur_seq;
+		
+			if (sendto(sockfd, buffer, PACKET_LEN, 0, (struct sockaddr*) &client_addr, slen) == -1)
+			{
+				perror("Error: could not send acknowledge to client\n");
+				close(sockfd);
+				exit(EXIT_FAILURE);
+			}
+			//Updating the expected sequence number.
+			exp_seq = (++exp_seq) % 2;
+		}
     }
  
     close(sockfd);
